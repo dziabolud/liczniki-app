88101df6d45f2177d2c5dce9eb7bc585e38acce1
dziabolud <plastikowywidelec@gmail.com>
2024-09-30 13:29:18 +0200

    Login blur

--- script.js
+++ script.js
@@ -9,15 +9,22 @@ function login() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
     const loginMessage = document.getElementById('login-message');
+    const overlay = document.getElementById('overlay');
 
     if (accounts[username] && accounts[username] === password) {
         loginMessage.innerText = 'Zalogowano pomyślnie!';
         loginMessage.style.color = 'green';
+
+        // Ukryj nakładkę po udanym logowaniu
+        overlay.style.display = 'none'; // Ustaw nakładkę jako niewidoczną
+        
         // Możesz dodać logikę po zalogowaniu, np. ukrycie formularza logowania
     } else {
         loginMessage.innerText = 'Błędna nazwa użytkownika lub hasło.';
     }
 }
+
+
 
 // Dodajemy obsługę kliknięcia dla przycisku logowania
 document.getElementById('login-button').addEventListener('click', login);
